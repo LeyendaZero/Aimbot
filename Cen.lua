05e3dd3344198bc07309c00d85c897b21409167f
LeyendaZero <aopenhimer@gmail.com>
2025-06-25 22:47:17 -0600

    Update Cen.lua

--- Cen.lua
+++ Cen.lua
@@ -4,7 +4,7 @@ local Workspace = game:GetService("Workspace")
 local LocalPlayer = Players.LocalPlayer
 local Camera = Workspace.CurrentCamera
 
-local bulletVelocity = 1100
+local bulletVelocity = 1000
 local fovRadius = 20
 local aimSmoothness = 0.50
 
